575a83062ed418fc7b52c2e0f15352e26dba1c7f
Harshit Gupta <harshitgupta1023@gmail.com>
2021-07-12 03:44:07 +0530

    Added DOB

---
+++ centuryApp/components/profile/ChooseDate.js
@@ -0,0 +1,63 @@
+import React, {useState, useCallback} from 'react';
+import {Icon} from 'react-native-elements';
+import {DatePickerModal} from 'react-native-paper-dates';
+
+import firestore from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
+import {showMessage} from 'react-native-flash-message';
+const ChooseDate = ({date, setDate}) => {
+  const [open, setOpen] = useState(false);
+
+  const onDismissSingle = useCallback(() => {
+    setOpen(false);
+  }, [setOpen]);
+
+  const onConfirmSingle = useCallback(
+    params => {
+      updateDate(params.date);
+      setOpen(false);
+      setDate(params.date);
+    },
+    [setOpen, setDate],
+  );
+
+  const updateDate = async newDate => {
+    try {
+      await firestore()
+        .collection('users')
+        .doc(auth().currentUser.uid)
+        .update({DOB: firestore.Timestamp.fromDate(newDate)});
+      showMessage({
+        message: 'DOB Updated',
+        description: 'Date of Birth was updated successfully!!',
+        type: 'success',
+      });
+    } catch (err) {
+      console.log(err);
+      showMessage({
+        message: 'ERROR !!!!!!!',
+        description: err.message,
+        type: 'danger',
+      });
+    }
+  };
+  return (
+    <>
+      <Icon name="edit" type="font-awesome" onPress={() => setOpen(true)} />
+
+      <DatePickerModal
+        mode="single"
+        visible={open}
+        onDismiss={onDismissSingle}
+        date={date}
+        onConfirm={onConfirmSingle}
+        validRange={{
+          startDate: new Date(1900, 1, 2), // optional
+          endDate: new Date(), // optional
+        }}
+      />
+    </>
+  );
+};
+
+export default ChooseDate;
--- centuryApp/screens/Profile.js
+++ centuryApp/screens/Profile.js
@@ -1,35 +1,45 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, StyleSheet, Image} from 'react-native';
+import {View, Text, StyleSheet, Image, Dimensions} from 'react-native';
 
 import {Button, Icon, Overlay, ListItem} from 'react-native-elements';
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
+import AppLoading from '../hooks/AppLoading';
 
 import Colors from '../constants/Colors';
 import Address from '../components/profile/Address';
 import Logo from '../assets/logo.png';
 
+import ChooseDate from '../components/profile/ChooseDate';
+import {ScrollView} from 'react-native-gesture-handler';
+
+const height = Dimensions.get('screen').height;
+const width = Dimensions.get('screen').width;
+
 const Profile = props => {
   const [isLoading, setIsLoading] = useState(true);
   const [oldAddress, setAddress] = useState({});
+  const [date, setDate] = useState();
   const [visible, setVisible] = useState(false);
+
   const toggleOverlay = () => {
     setVisible(!visible);
   };
+
+  const fetch = async () => {
+    var userData = await firestore()
+      .collection('users')
+      .doc(auth().currentUser.uid)
+      .get();
+    userData = userData.data();
+    setAddress(userData.address);
+    if (userData.DOB) setDate(userData.DOB.toDate());
+  };
+
   useEffect(() => {
     if (!auth().currentUser) {
       props.navigation.navigate('Login');
     } else {
-      const fetch = async () => {
-        var userData = await firestore()
-          .collection('users')
-          .doc(auth().currentUser.uid)
-          .get();
-        userData = userData.data();
-        setAddress(userData.address);
-        setIsLoading(false);
-      };
-      fetch();
       const onResult = () => setIsLoading(true);
       const unsubscribe = firestore()
         .collection('users')
@@ -37,14 +47,19 @@ const Profile = props => {
         .onSnapshot(onResult, console.warn);
       return () => unsubscribe();
     }
-  });
-  if (!auth().currentUser || isLoading) {
+  }, []);
+  if (isLoading) {
     return (
-      <View>
-        <Text>Redirecting...</Text>
-      </View>
+      <AppLoading
+        fetchItems={fetch}
+        onFinish={() => {
+          setIsLoading(!isLoading);
+        }}
+        onError={console.warn}
+      />
     );
   }
+
   const renderList = [
     {
       name: auth().currentUser.displayName
@@ -53,6 +68,13 @@ const Profile = props => {
       title: 'Name',
       type: 'fontAwesome',
       icon: 'account-box',
+    },
+    {
+      name: date ? date.toDateString() : '(No DOB)',
+      title: 'DOB',
+      type: 'fontAwesome',
+      icon: 'date-range',
+      leftIcon: 'edit',
     },
     {
       name: oldAddress.address ? oldAddress.address : '(Not present)',
@@ -82,31 +104,41 @@ const Profile = props => {
   return (
     <View style={styles.screen}>
       <Image source={Logo} style={styles.image} />
-      {renderList.map((dat, idx) => (
-        <ListItem key={idx} bottomDivider style={{width: '95%'}}>
-          <ListItem.Content
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'flex-start',
-            }}>
-            <Icon name={dat.icon} type={dat.type} />
-            <ListItem.Title style={{marginLeft: 20}}>
-              {dat.title}
-            </ListItem.Title>
-          </ListItem.Content>
-          <ListItem.Content>
-            <ListItem.Subtitle>{dat.name}</ListItem.Subtitle>
-          </ListItem.Content>
-        </ListItem>
-      ))}
-      <View style={styles.buttonContainer}>
-        <Button
-          title="Update Profile"
-          onPress={toggleOverlay}
-          buttonStyle={styles.button}
-          titleStyle={styles.titleButton}
-        />
-      </View>
+      <ScrollView
+        style={{
+          width: '100%',
+        }}
+        contentContainerStyle={{
+          alignItems: 'center',
+        }}>
+        {renderList.map((dat, idx) => (
+          <ListItem key={idx} bottomDivider style={{width: '95%'}}>
+            <ListItem.Content
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'flex-start',
+              }}>
+              <Icon name={dat.icon} type={dat.type} />
+              <ListItem.Title style={{marginLeft: 20}}>
+                {dat.title}
+              </ListItem.Title>
+            </ListItem.Content>
+            <ListItem.Content>
+              <ListItem.Subtitle>{dat.name}</ListItem.Subtitle>
+            </ListItem.Content>
+            {dat.leftIcon ? <ChooseDate date={date} setDate={setDate} /> : null}
+          </ListItem>
+        ))}
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Update Profile"
+            onPress={toggleOverlay}
+            buttonStyle={styles.button}
+            titleStyle={styles.titleButton}
+          />
+        </View>
+      </ScrollView>
+
       <Overlay
         isVisible={visible}
         onBackdropPress={toggleOverlay}
@@ -128,15 +160,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   image: {
-    width: 150,
-    height: 150,
+    width: width / 2,
+    height: height / 5,
     marginBottom: 10,
   },
   buttonContainer: {
     overflow: 'hidden',
-    borderWidth: 1,
     borderRadius: 30,
-    width: '75%',
+    width: '65%',
     marginTop: 20,
   },
   button: {
